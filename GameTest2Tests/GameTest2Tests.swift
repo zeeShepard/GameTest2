292bbfab153d110fb1689107d22220ae6241c8a4
Zack Shepard <Zack@johns-mbp.lan>
2016-01-21 18:13:42 -0700

    Initial Commit

---
+++ GameTest2Tests/GameTest2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GameTest2Tests.swift
+//  GameTest2Tests
+//
+//  Created by Zack Shepard on 1/21/16.
+//  Copyright © 2016 Zack Shepard. All rights reserved.
+//
+
+import XCTest
+@testable import GameTest2
+
+class GameTest2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
